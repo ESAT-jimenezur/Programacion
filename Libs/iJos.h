f426464c23c3806ceeb054f610077239973fa4f5
ESAT-jimenezur <jimenezur@esat-alumni.com>
2013-11-25 19:54:00 +0100

    Añadiendo librería propia

---
+++ Libs/iJos.h
@@ -0,0 +1,58 @@
+/*--------------------------------*/
+/*        iJos Public Lib         */
+/*--------------------------------*/
+
+
+
+/**       Funcion Sumar      **/
+/**--------------------------------
+    @Params float a;
+            float b;
+    @return float a + b
+----------------------------------*/
+float sumar(float a, float b){
+    return a + b;
+}
+
+
+/**       Funcion Restar      **/
+/**--------------------------------
+    @Params float a;
+            float b;
+    @return float a - b
+----------------------------------*/
+float restar(float a, float b){
+    return a - b;
+}
+
+
+/**       Funcion Factorial      **/
+/**--------------------------------
+    @Params float a;
+    @return float a * factorial(a-1)
+----------------------------------*/
+float factorial(float a){
+    if(a == 1){
+        return 1;
+    }else{
+        return a * factorial(a - 1);
+    }
+}
+
+
+/**       Funcion Potencia      **/
+/**--------------------------------
+    @Params float a;
+            float b
+    @return float c
+----------------------------------*/
+float potencia(float a, float b){
+    if(b == 1){
+        return a;
+    }else{
+        return a * potencia(a, b-1);
+    }
+}
+
+
+
