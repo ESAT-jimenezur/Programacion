35e448ec485ce4eac13d29b646245803e804c81d
ESAT-jimenezur <jimenezur@esat-alumni.com>
2013-12-04 19:50:58 +0100

    Continuacion de 3 en raya

--- 3 En Raya/TresEnRaya.cpp
+++ 3 En Raya/TresEnRaya.cpp
@@ -14,6 +14,22 @@ void setColors(int texto, int fondo){
 
 }
 
+void ponerFicha(int turno){
+    int posicion;
+    if(turno == -1){// Turno PC
+        printf("Turno del ORDENADOR \n");
+        scanf("%d", &posicion);
+         while (posicion < 1|| posicion > 9){
+
+         };
+         posicion--;
+         printf("_______________________________\n");
+         //AjustaTurno(casilla);
+    }else{
+
+    }
+
+}
 
 void juego(){
 
@@ -21,7 +37,9 @@ void juego(){
     bool debug = true;
     int jugador = 2;
     int ordenador = 10;
-    int turno = 0; // Turno 0 = ordenador, 1 = persona
+    int turno = -1; // Turno 0 = ordenador, 1 = persona
+    int fichasOrdenador = 3;
+    int fichasPersona = 3;
 
     int tablero[4][4] = {       // Array  tablero (Ultima fila y columna solo son para debuguear)
                             0,0,0,0,
@@ -30,30 +48,18 @@ void juego(){
                             0,0,0,0
                         };
 
-     int letrasTablero [4][4] = {       // Array  tablero (Ultima fila y columna solo son para debuguear)
+    // Se le llamó "letras Tablero" pero la idea después fue utilizar el teclado numérico para jugar
+    int letrasTablero [4][4] = {       // Array  tablero (Ultima fila y columna solo son para debuguear)
                             7, 8, 9,
                             4, 5, 6,
                             1, 2, 3
                         };
-
+    int ale;
 
     // Establecemos el color de la consola
     setColors(0, 14);
 
     do{
-
-
-        if(turno == 0){ // Turno  == 0 -> Ordenador
-
-            tablero[2][2] = ordenador;
-
-        }else{ // Turno  == 1 -> Persona
-
-
-
-        }
-
-
 
 
 
@@ -68,7 +74,7 @@ void juego(){
         }
 
 
-
+        // Muestra el debug (Si está activo)
         if(debug){
 
             printf("\n\n\n");
@@ -83,6 +89,8 @@ void juego(){
             }
         }
 
+        ponerFicha(turno);
+
 
         printf("---------------\n");
         if(turno == 0){ // Turno  == 0 -> Ordenador
@@ -92,10 +100,14 @@ void juego(){
         }
 
 
-
-        partidaActiva = false;
-
-
+        //                 COMBPROBAMOS SUMA HORIZONTAL         ||          COMPROBAMOS SUMA VERTICAL                  ||           COMPROBAMOS SUMA VERTICAL
+        if( tablero[0][0] + tablero[0][1] + tablero[0][2] == 30 || tablero[0][0] + tablero[1][0] + tablero[2][0] == 30 || tablero[0][0] + tablero[1][1] + tablero[2][2] == 30){
+            printf("Gana ordenador");
+            partidaActiva = false;
+        }else{
+            printf("Gana persona");
+            partidaActiva = false;
+        }
 
 
     }while(partidaActiva);
@@ -105,7 +117,7 @@ void juego(){
 
 int main(){
 
-    juego();
+    //juego();
 
 }
 
