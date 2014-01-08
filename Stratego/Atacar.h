f7951a9b56b704c84f4dbfc1020b5a86cadbae8e
ESAT-jimenezur <jimenezur@esat-alumni.com>
2014-01-08 19:34:09 +0100

    Añadido prototipo de la función Atacar
    Implementada en una librería externa, para su mejor inclusion, luego en
    el proyecto.

---
+++ Stratego/Atacar.h
@@ -0,0 +1,18 @@
+
+
+/**************************************
+ ** Este metodo es utilizado por las **
+ ** fichas para atacar a otras       **
+ **                                  **
+ ** GET                              **
+ ** int atacante (El tipo de ficha)  **
+ ** int receptor (El tipo de ficha)  **
+ ** RETURN                           **
+ ** int ganador (La ficha que gana)  **
+**************************************/
+
+int atacar(int atacante, int receptor){
+    int ganador;
+
+    return ganador;
+}
