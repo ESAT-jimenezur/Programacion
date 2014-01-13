9fa93ccf2dc761e2c1806d8a949a01bff1e98750
ESAT-jimenezur <jimenezur@esat-alumni.com>
2014-01-13 19:06:10 +0100

    Metodo Atacar, pasado al codigo principal

--- Stratego/Stratego.cpp
+++ Stratego/Stratego.cpp
@@ -219,6 +219,53 @@ void movimientoCursor(){
 
 }
 
+
+
+/**************************************
+ ** Este metodo es utilizado por las **
+ ** fichas para atacar a otras       **
+ **                                  **
+ ** @get                             **
+ ** int atacante (El tipo de ficha)  **
+ ** int receptor (El tipo de ficha)  **
+ ** @return                          **
+ ** int ganador (La ficha que gana)  **
+**************************************/
+
+int atacar(int atacante, int receptor){
+    bool ganador;
+
+    //Definimos los tipos de batalla, para saber quien gana.
+
+    /** TODO
+        - Comprobar que los valores sean correctos
+        - Comprobar el caso de partida ganada
+    **/
+
+    if(atacante == 1 && receptor == 12){ // En este caso, el atacante, usando el espia ha atacado al mariscal.
+        ganador = 1; // Gana el atacante
+    }
+
+
+    if(receptor == 100){ // Si el atacante toca la bandera
+        ganador = 1;
+        //TODO
+        //partidaGanada = true;
+    }
+
+    if(receptor == 99){ // 99 Es la bomba
+        if(atacante == 3){ // Tenemos un minero desactivador de bombas!
+            ganador = 1; // Bomba desactivada!
+        }else{
+            ganador = 0; // Te has chocado con la bomba, amigo!
+        }
+    }
+
+    return ganador;
+}
+
+
+
 void modoDebug(){
 
     if(debug){
--- Stratego/Utils.h
+++ Stratego/Utils.h
@@ -1,3 +1,6 @@
+// Este fichero tiene que estar incluido en la carpeta del Stratego (junto a Stratego.cpp)
+// Ademas tendremos que configurarlo en el compilador.
+
 
 /**************************************************
     Metodo que devuelve un error, según su codigo
@@ -26,45 +29,5 @@ void nombreFicha(int ficha){
 }
 
 
-/**************************************
- ** Este metodo es utilizado por las **
- ** fichas para atacar a otras       **
- **                                  **
- ** @get                             **
- ** int atacante (El tipo de ficha)  **
- ** int receptor (El tipo de ficha)  **
- ** @return                          **
- ** int ganador (La ficha que gana)  **
-**************************************/
 
-int atacar(int atacante, int receptor){
-    bool ganador;
-
-    //Definimos los tipos de batalla, para saber quien gana.
-
-    /** TODO
-        - Comprobar que los valores sean correctos
-        - Comprobar el caso de partida ganada
-    **/
-
-    if(atacante == 1 && receptor == 12){ // En este caso, el atacante, usando el espia ha atacado al mariscal.
-        ganador = 1; // Gana el atacante
-    }
-
-
-    if(receptor == 100){ // Si el atacante toca la bandera
-        ganador = 1;
-        partidaGanada = true;
-    }
-
-    if(receptor == 99){ // 99 Es la bomba
-        if(atacante == 3){ // Tenemos un minero desactivador de bombas!
-            ganador = 1; // Bomba desactivada!
-        }else{
-            ganador = 0; // Te has chocado con la bomba, amigo!
-        }
-    }
-
-    return ganador;
-}
 
